c569a8ab6069f60d1ea341c0ed0e3e658c1744b3
TejasYesurkar <tejasyesurkar0@gmail.com>
2020-10-12 17:04:18 +0530

    first commit

---
+++ mysnackbarlibrary/src/main/java/com/dagduteliretail/mysnackbarlibrary/MySnackBar.java
@@ -0,0 +1,10 @@
+package com.dagduteliretail.mysnackbarlibrary;
+
+import android.util.Log;
+
+public class MySnackBar {
+    private static final String TAG = "My_AWESOME_LOG";
+    public static void easyLog(String message){
+        Log.d(TAG,message);
+    }
+}
